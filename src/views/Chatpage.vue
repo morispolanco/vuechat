c40901c94fdb1bd908f97d37f01a25f74b1f41fc
morispolanco <morispolanco@gmail.com>
2023-07-27 22:20:53 -0600

    Create Chatpage.vue

---
+++ src/views/Chatpage.vue
@@ -0,0 +1,16 @@
+<!-- src/views/ChatPage.vue -->
+<template>
+  <div>
+    <Chatbot />
+  </div>
+</template>
+
+<script>
+import Chatbot from '@/components/Chatbot.vue';
+
+export default {
+  components: {
+    Chatbot,
+  },
+};
+</script>
